5649c5a3cb38be40fd51628d2c24408e4b3a8526
rvpacheco <124650306+rvpacheco@users.noreply.github.com>
2023-05-11 03:00:15 +0000

    first commit

--- src/models.py
+++ src/models.py
@@ -7,23 +7,37 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class People(Base):
+    __tablename__ = 'People'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    gender = Column(String(20), nullable=False)
+    height = Column(String(20), nullable=True)
+    mass = Column(String(20), nullable=True)
+    hair_color = Column(String(250), nullable=True)
+    skin_color = Column(String(250), nullable=True)
+    homeworld = Column(String(250), nullable=True)
+    planet = relationship("Planets")
+    films = relationship("Films")
 
-class Address(Base):
-    __tablename__ = 'address'
+class Cast(Base):
+    __tablename__ = 'cast'
+    id= Column(Integer, primary_key=True)
+    film_id= Column(Integer, ForeignKey("films.id"))
+    film=relationship("Films", backref= "cast")
+    people_id=Column(Integer,ForeignKey("people.id"))
+    planet=relationship("Planets") 
+
+class Films(Base):
+    __tablename__ = 'Films'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    episode_id = Column(Integer, primary_key=True)
+    producer = Column(String(250),nullable=True)
+    release_date = Column(Integer, nullable=True)
+    title = Column(String(100),nullable=False)
 
     def to_dict(self):
         return {}
