467f137b761ca7a7066039ac5fa134844b643a49
rvpacheco <124650306+rvpacheco@users.noreply.github.com>
2023-05-17 04:05:02 +0000

    Terminado

--- src/models.py
+++ src/models.py
@@ -8,7 +8,7 @@ from eralchemy2 import render_er
 Base = declarative_base()
 
 class People(Base):
-    __tablename__ = 'People'
+    __tablename__ = 'people'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
@@ -18,9 +18,21 @@ class People(Base):
     mass = Column(String(20), nullable=True)
     hair_color = Column(String(250), nullable=True)
     skin_color = Column(String(250), nullable=True)
-    homeworld = Column(String(250), nullable=True)
-    planet = relationship("Planets")
-    films = relationship("Films")
+    homeworld_id = Column(Integer, ForeignKey("planets.id"))
+    people = relationship("Films")
+    vehicles = relationship("Vehicles")
+
+class Films(Base):
+    __tablename__ = 'films'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    title = Column(String(250))
+    director = Column(String(100))
+    episode_id = Column(String(100),nullable=False)
+    people_id = Column(Integer, ForeignKey('people.id'))
+    Cast = relationship ("cast")
+    Vehicles = relationship ("vehicles")
 
 class Cast(Base):
     __tablename__ = 'cast'
@@ -28,19 +40,35 @@ class Cast(Base):
     film_id= Column(Integer, ForeignKey("films.id"))
     film=relationship("Films", backref= "cast")
     people_id=Column(Integer,ForeignKey("people.id"))
-    planet=relationship("Planets") 
+    people=relationship("Planets",backref="people")
+    planet_id=relationship(Integer,ForeignKey("planet.id"))
+    planet=relationship("Planets",backref="cast") 
+    
+class Vehicles(Base):
+    __tablename__ = 'vehicles'
+    id = Column(Integer, primary_key=True)
+    model= Column(String(250), nullable=False)
+    vehicle_class= Column(String(250), nullable=False)
+    passengers = Column(String(250), nullable=False)
+    films_id = Column(Integer, ForeignKey('films.id')) 
+    pilots_id = Column(Integer, ForeignKey('people.id'))
+    Cast = relationship ("cast")
 
-class Films(Base):
-    __tablename__ = 'Films'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    episode_id = Column(Integer, primary_key=True)
-    producer = Column(String(250),nullable=True)
-    release_date = Column(Integer, nullable=True)
-    title = Column(String(100),nullable=False)
+class Planets(Base):
+    __tablename__= "planets"
+    id = Column(Integer, primary_key=True)
+    climate= Column(String(250), nullable=False)
+    name = Column(String(250), nullable=False)
+    population = Column(String(250), nullable=False)
+    people = relationship ("people")
+    Cast = relationship ("cast")
 
-    def to_dict(self):
-        return {}
+
+
+
+def to_dict(self):
+ return {}
+
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
