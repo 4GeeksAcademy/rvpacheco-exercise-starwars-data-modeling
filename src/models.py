ac95ace1fc763b28b47638be2cdc5e0a0409aeba
rvpacheco <124650306+rvpacheco@users.noreply.github.com>
2023-05-18 16:06:59 +0000

    Termiando favorites y user

--- src/models.py
+++ src/models.py
@@ -1,11 +1,18 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String,Boolean
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
+
+class User(Base):
+    __tablename__="users"
+    id = Column(Integer, primary_key=True)
+    email = Column(String(120),unique=True,nullable = False)
+    password = Column(String(120),unique=False,nullable = False)
+    is_active = Column(Boolean(),unique=False, nullable=False)
 
 class People(Base):
     __tablename__ = 'people'
@@ -55,7 +62,7 @@ class Vehicles(Base):
     Cast = relationship ("cast")
 
 class Planets(Base):
-    __tablename__= "planets"
+    __tablename__= 'planets'
     id = Column(Integer, primary_key=True)
     climate= Column(String(250), nullable=False)
     name = Column(String(250), nullable=False)
@@ -63,7 +70,14 @@ class Planets(Base):
     people = relationship ("people")
     Cast = relationship ("cast")
 
-
+class Favorites(Base):
+    __tablename__= 'favorites'
+    id = Column(Integer,primary_key=True)
+    type = Column(String(20), nullable=False)
+    element_id = Column(Integer, nullable=False)
+    user_id=Column(Integer, ForeignKey("users.id"))
+    user = relationship(User)
+    
 
 
 def to_dict(self):
